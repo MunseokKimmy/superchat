b3572646495005a5b144a283591e32018cf42a3b
Munseok Kim <59575386+PresideMoon@users.noreply.github.com>
2022-04-19 17:17:46 -0600

    Add chatroom functions.

--- src/App.js
+++ src/App.js
@@ -8,21 +8,64 @@ import 'firebase/auth';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
 
-firebase.initializeApp({
-  //your config 
-})
+const firebaseConfig = {
+  apiKey: "AIzaSyA8VA5lETYBAJuK3Ewxyl1AapLIPWPeFS0",
+  authDomain: "superchat-4164d.firebaseapp.com",
+  projectId: "superchat-4164d",
+  storageBucket: "superchat-4164d.appspot.com",
+  messagingSenderId: "845292735645",
+  appId: "1:845292735645:web:b8aca44a9838e944dd9874"
+};
 
 const auth = firebase.auth();
 const firestore = firebase.firestore();
 
 function App() {
+  const [user] = useAuthState(auth);
   return (
     <div className="App">
-      <header className="App-header">
+      <header>
 
       </header>
+      <section>
+        {user ? <ChatRoom /> : <SignIn />}
+      </section>
     </div>
   );
 }
 
+function SignIn() {
+  const signInWithGoogle = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider);
+  }
+
+  return (
+    <button onClick={signInWithGoogle}>Sign in with Google</button>
+  )
+}
+
+function SignOut() {
+  return auth.currentUser && (
+    <button onClick={() => auth.signOut()}>Sign Out</button>
+  )
+}
+
+function ChatRoom() {
+  const messagesRef = firestore.collection('messages');
+  const query = messagesRef.orderBy('createdAt').limit(25);
+  const [messages] = useCollectionData(query, {idField: 'id'});
+  return (
+    <>
+      <div>
+        {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
+      </div>
+    </>
+  )
+}
+
+function ChatMessage(props) {
+  const { text, uid } = props.message;
+  return <p>{text}</p>
+}
 export default App;
